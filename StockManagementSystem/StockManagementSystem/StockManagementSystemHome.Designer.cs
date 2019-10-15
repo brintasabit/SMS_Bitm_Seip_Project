410a71eef7862f3157679fbf6ea1de3e179d009d
brintasabit <brintasabit@gmail.com>
2019-10-15 18:09:24 +0600

    All Four Forms Created

---
+++ StockManagementSystem/StockManagementSystem/StockManagementSystemHome.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace StockManagementSystem
+{
+    partial class StockManagementSystemHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonCategory = new System.Windows.Forms.Button();
+            this.ButtonProduct = new System.Windows.Forms.Button();
+            this.ButtonCustomer = new System.Windows.Forms.Button();
+            this.ButtonSupplier = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonCategory
+            // 
+            this.ButtonCategory.Location = new System.Drawing.Point(98, 104);
+            this.ButtonCategory.Name = "ButtonCategory";
+            this.ButtonCategory.Size = new System.Drawing.Size(249, 86);
+            this.ButtonCategory.TabIndex = 0;
+            this.ButtonCategory.Text = "Product Catalog Module (Category)";
+            this.ButtonCategory.UseVisualStyleBackColor = true;
+            this.ButtonCategory.Click += new System.EventHandler(this.ButtonCategory_Click);
+            // 
+            // ButtonProduct
+            // 
+            this.ButtonProduct.Location = new System.Drawing.Point(407, 104);
+            this.ButtonProduct.Name = "ButtonProduct";
+            this.ButtonProduct.Size = new System.Drawing.Size(249, 86);
+            this.ButtonProduct.TabIndex = 1;
+            this.ButtonProduct.Text = "Product Catalog Module (Product)";
+            this.ButtonProduct.UseVisualStyleBackColor = true;
+            this.ButtonProduct.Click += new System.EventHandler(this.ButtonProduct_Click);
+            // 
+            // ButtonCustomer
+            // 
+            this.ButtonCustomer.Location = new System.Drawing.Point(98, 250);
+            this.ButtonCustomer.Name = "ButtonCustomer";
+            this.ButtonCustomer.Size = new System.Drawing.Size(249, 86);
+            this.ButtonCustomer.TabIndex = 2;
+            this.ButtonCustomer.Text = "Party Module (Customer)";
+            this.ButtonCustomer.UseVisualStyleBackColor = true;
+            this.ButtonCustomer.Click += new System.EventHandler(this.ButtonCustomer_Click);
+            // 
+            // ButtonSupplier
+            // 
+            this.ButtonSupplier.Location = new System.Drawing.Point(407, 250);
+            this.ButtonSupplier.Name = "ButtonSupplier";
+            this.ButtonSupplier.Size = new System.Drawing.Size(249, 86);
+            this.ButtonSupplier.TabIndex = 3;
+            this.ButtonSupplier.Text = "Party Module (Supplier)";
+            this.ButtonSupplier.UseVisualStyleBackColor = true;
+            this.ButtonSupplier.Click += new System.EventHandler(this.ButtonSupplier_Click);
+            // 
+            // StockManagementSystemHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 427);
+            this.Controls.Add(this.ButtonSupplier);
+            this.Controls.Add(this.ButtonCustomer);
+            this.Controls.Add(this.ButtonProduct);
+            this.Controls.Add(this.ButtonCategory);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "StockManagementSystemHome";
+            this.Text = "Stock Management System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonCategory;
+        private System.Windows.Forms.Button ButtonProduct;
+        private System.Windows.Forms.Button ButtonCustomer;
+        private System.Windows.Forms.Button ButtonSupplier;
+    }
+}
+
---
+++ StockManagementSystem/StockManagementSystem/StockManagementSystemHome.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockManagementSystem
+{
+    public partial class StockManagementSystemHome : Form
+    {
+        ProductCatalogModuleCategory pcmc=new ProductCatalogModuleCategory();
+        ProductCatalogModuleProduct pcmp=new ProductCatalogModuleProduct();
+        PartyModuleCustomer pmc=new PartyModuleCustomer();
+        PartyModuleSupplier pms=new PartyModuleSupplier();
+        public StockManagementSystemHome()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCategory_Click(object sender, EventArgs e)
+        {
+            if (pcmc.IsDisposed)
+            {
+                pcmc=new ProductCatalogModuleCategory();
+            }
+            pcmc.Show();
+            pcmc.BringToFront();
+        }
+
+        private void ButtonProduct_Click(object sender, EventArgs e)
+        {
+            if (pcmp.IsDisposed)
+            {
+                pcmp=new ProductCatalogModuleProduct();
+            }
+            pcmp.Show();
+            pcmp.BringToFront();
+        }
+
+        private void ButtonSupplier_Click(object sender, EventArgs e)
+        {
+            if (pms.IsDisposed)
+            {
+                pms=new PartyModuleSupplier();
+            }
+            pms.Show();
+            pms.BringToFront();
+        }
+
+        private void ButtonCustomer_Click(object sender, EventArgs e)
+        {
+            if (pmc.IsDisposed)
+            {
+                pmc=new PartyModuleCustomer();
+            }
+            pmc.Show();
+            pmc.BringToFront();
+        }
+    }
+}

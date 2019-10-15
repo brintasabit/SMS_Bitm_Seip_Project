281bc5adb7961beeb550067b0d4868d9c1b2fff3
brintasabit <brintasabit@gmail.com>
2019-10-16 03:41:39 +0600

    Final Error Free Code 16-10-19:3.41am

--- Sql/SQLQueryBMS.sql
+++ Sql/SQLQueryBMS.sql
@@ -1,11 +1,14 @@
-Create Database BMS
+Create Database StockManagementSystem
 create table Category(ID int identity (1,1),Code varchar(50),Name varchar(50))
-select * from Category
-insert into Category values('BA-23','Mobile')
-
-
-
-
+select * from Category where Code='CA-23'
+insert into Category values('-Select-','-Select-')
+insert into Category values('CA-23','Electronics')
+create table Product(ID int identity (1,1),Category varchar(50),Code varchar(50),Name varchar(50),ReOrderLevel varchar(50),Description varchar(100))
+select * from Product
+insert into Product values('Electronics','BA-23','Mobile','23','Quality is fine')
+select Name from Category
+drop table Category
+drop table Product
 
 
 
